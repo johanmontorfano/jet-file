7e30d97002c3e4803e2cd642232a13642c51f475
Johan <me@johanmontorfano.com>
2023-01-14 20:49:40 +0100

    namespace fixes

--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@ import { JetStore } from "./store";
 import { JetWrapper } from "./wrapper";
 
 export namespace Jet {
-    const File = JetFile;
-    const Store = JetStore;
-    const Wrapper = JetWrapper;
-    const labels = Object.freeze({
+    export const File = JetFile;
+    export const Store = JetStore;
+    export const Wrapper = JetWrapper;
+    export const labels = Object.freeze({
         store: "js",
         file: "jf"
     });
