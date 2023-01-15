49b6ca2695543cb37eaed8228b8afa3074cfeed4
Johan <me@johanmontorfano.com>
2023-01-15 16:49:55 +0100

    added JF to JS conversion utility

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import { JetFile } from "./database";
 import { JetStore } from "./store";
 import { JetWrapper } from "./wrapper";
+import { jfToJS } from "./tools/jf-to-js";
 
-export { JetFile, JetStore, JetWrapper }
+export { JetFile, JetStore, JetWrapper, jfToJS }
 export const JetWrapperLabels = Object.freeze({
      store: "js",
      file: "jf"
