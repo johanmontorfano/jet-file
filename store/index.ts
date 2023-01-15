b5a282605f79e9dbe1614e228cd7b15642d77276
Johan <me@johanmontorfano.com>
2023-01-15 17:36:13 +0100

    solved an issue with getEntries not properly unhashing keys

--- store/index.ts
+++ store/index.ts
@@ -47,7 +47,7 @@ export class JetStore<STORE_TYPE extends { [key: string]: any }> {
     /** This function removes the hash of a key store */
     private static unhashing(hash: string) {
         const uncryptedButStillWithCustomKeys = Buffer.from(hash, "hex").toString("utf-8").replace("v2s:", "");
-        return uncryptedButStillWithCustomKeys.split("").splice(0, uncryptedButStillWithCustomKeys.length - 2).join("");
+        return uncryptedButStillWithCustomKeys.split("").reverse().join("").replace("k.", "").split("").reverse().join("");
     }
 
     /** Get all the keys entries of this instance, to do this it gets all the files/folders
